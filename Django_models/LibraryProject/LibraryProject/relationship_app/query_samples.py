779f63bf00dd1108b5f51b814cba99446a77e410
AsmaaEissa <82784950+Asmaatef@users.noreply.github.com>
2024-11-22 14:49:04 +0200

    Update query_samples.py

--- Django_models/LibraryProject/LibraryProject/relationship_app/query_samples.py
+++ Django_models/LibraryProject/LibraryProject/relationship_app/query_samples.py
@@ -1,5 +1,4 @@
 from relationship_app.models import Author, Book, Library, Librarian
-
 def get_books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
@@ -9,7 +8,6 @@ def get_books_by_author(author_name):
             print(f"- {book.title}")
     except Author.DoesNotExist:
         print(f"Author '{author_name}' does not exist.")
-
 def get_books_in_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
@@ -19,8 +17,6 @@ def get_books_in_library(library_name):
             print(f"- {book.title}")
     except Library.DoesNotExist:
         print(f"Library '{library_name}' does not exist.")
-
-
 def get_librarian_for_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
