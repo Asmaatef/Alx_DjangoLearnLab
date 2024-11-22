730755dbf4eb9bbaea24cfda226edeff61e08b60
AsmaaEissa <82784950+Asmaatef@users.noreply.github.com>
2024-11-22 14:46:34 +0200

    Add files via upload

---
+++ Django_models/LibraryProject/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,32 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+def get_books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        books = author.books.all()
+        print(f"Books by {author.name}:")
+        for book in books:
+            print(f"- {book.title}")
+    except Author.DoesNotExist:
+        print(f"Author '{author_name}' does not exist.")
+
+def get_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        books = library.books.all()
+        print(f"Books in {library.name} library:")
+        for book in books:
+            print(f"- {book.title}")
+    except Library.DoesNotExist:
+        print(f"Library '{library_name}' does not exist.")
+
+
+def get_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        librarian = library.librarian  
+        print(f"Librarian for {library.name} library: {librarian.name}")
+    except Library.DoesNotExist:
+        print(f"Library '{library_name}' does not exist.")
+    except Librarian.DoesNotExist:
+        print(f"No librarian assigned to {library_name} library.")
