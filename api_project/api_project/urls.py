940a59b1f5978ff0150cae6ef2adf4f8a8247fe8
AsmaaEissa <82784950+Asmaatef@users.noreply.github.com>
2024-12-09 15:50:51 +0200

    Update urls.py

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -16,10 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  
+]
+
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
 
 
