a3ca359c9a317bcc2c38f90c91ed2fed04f39a7d
AsmaaEissa <82784950+Asmaatef@users.noreply.github.com>
2024-12-09 15:41:43 +0200

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,13 @@ from django.urls import path
 from .views import BookList
 generics.ListAPIView 
 from .serializers import BookSerializer
+from rest_framework.viewsets import ModelViewSet
+from .models import Book
+
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
 ]
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
