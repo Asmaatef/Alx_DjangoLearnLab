94ddda00e690ffa39029b0a37583e72852f87f87
AsmaaEissa <82784950+Asmaatef@users.noreply.github.com>
2024-12-09 15:34:25 +0200

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.urls import path
+from .views import BookList
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+]
